d13f815c62f21d772fc83f20ffe97cf53e5033cc
Miriam Cortes <mcortes.umiami@gmail.com>
2016-11-23 20:46:08 -0800

    holy crap it works

--- scripts.js
+++ scripts.js
@@ -1,33 +1,51 @@
 //scripts.js
+$(document).ready(function() {
 
-var url = 'https://trektravel.herokuapp.com/trips';
-var successCallback = function(response) {
-  console.log(response);
-  for (var i = 0; i < response.length; i++) {
-    $('#trips').append(
-      '<td class="small-12 medium-6 large-4 column"><a href=' + url + "/" + response[i].id + 'id="modal-launcher">' + response[i].name + "</a></td>");
-  }
-};
+  var url = 'https://trektravel.herokuapp.com/trips';
 
-//button click to get all trip options
-$('#all').click(function(event) {
-  $.get(url, successCallback)
-});
+  $('form').submit(function(e) {
+    e.preventDefault();
+    var theTripsID = $('#theTripsID').attr('value')
 
+    var url = "https://trektravel.herokuapp.com/trips/" + theTripsID + "/reserve";
+    var formData = $(this).serialize();
+    console.log("theTripsID: " + theTripsID);
 
-//clicking on individual trips
-$('#trips').on('click', 'a',function(e) {
-  e.preventDefault();
-  console.log("I'm preventing the default");
+    $.post(url,formData,function(response) {
+      $('#modal-content').hide()
+      $('#message').html('<p> Your spot has been reserved! </p>');
 
-  $('#modal-launcher').show();
-  var tripUrl = $(this).attr('href')
-
-  $.get(tripUrl, function(trip){
-
-    $("#modal-launcher, #modal-background, #modal-close").click(function () {
+      console.log(theTripsID);
+    })
+  })
 
 
+
+
+
+
+  var successCallback = function(response) {
+    console.log(response);
+    for (var i = 0; i < response.length; i++) {
+      $('#trips').append(
+        '<td class="small-12 medium-6 large-4 column"><a href=' + url + "/" + response[i].id + 'id="modal-launcher">' + response[i].name + "</a></td>");
+    }
+  };
+
+  //button click to get all trip options
+  $('#all').click(function(event) {
+    $.get(url, successCallback)
+  });
+
+  //clicking on individual trips
+  $('#trips').on('click', 'a',function(e) {
+    e.preventDefault(); //console.log("I'm preventing the default");
+
+    $('#modal-content').show(); //the pop-up window
+    var tripUrl = $(this).attr('href')
+
+    $.get(tripUrl, function(trip){
+      // this should make the modal pop up
       console.log(trip);
       $('#name').text(trip.name)
       $('#destination').text("Destination: " + trip.destination);
@@ -37,17 +55,20 @@ $('#trips').on('click', 'a',function(e) {
       $('#cost').text("$" + trip.cost);
       $('#tripid').text("Trip id: " + trip.id);
       $('#about').text(trip.about);
+      $('#theTripsID').attr('value',trip.id);
+      console.log($('#theTripsID').attr('value'));
+      // modal close button
+      $("#modal-content").toggleClass("active");
+      $("#modal-close").click(function() {
+        $('#modal-content').hide()
+      })
 
-
-      $("#modal-content,#modal-background").toggleClass("active");
-    });
-
-
-  }).always(function() {
-    $('message').text("something happened.");
-    console.log("something happened.");
-  }).fail(function() {
-    alert("this failed")
-  })
-
+    // give me responses no matter what happens...
+    }).always(function() {
+      $('message').text("something happened.");
+      console.log("something happened.");
+    }).fail(function() {
+      alert("this failed")
+    })
+  });
 });
